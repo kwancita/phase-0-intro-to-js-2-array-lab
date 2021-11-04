2cecce2fba611eaec1f645c14d683e902f5ae990
Wasanta <kwan_u_day@hotmail.com>
2021-11-04 12:45:43 -0400

    commit tree 7ca29163c557afeb3bdab9493937575ad57f2c7a

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = [];
+cats.push('Milo', 'Otis', 'Garfield');
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    var appendCat = [...cats,"Broom"];
+    return appendCat;
+}
+function prependCat(name){
+    var prependCat = ['Arnold',...cats,];
+    return prependCat;
+}
+function removeLastCat(){
+    var removeLastCat= cats.slice(0,cats.length-1)
+    return removeLastCat;
+}
+function removeFirstCat(){
+    var removeFirstCat= cats.slice(1)
+    return removeFirstCat;
+}
+
